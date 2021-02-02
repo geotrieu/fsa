304e68b78b0085449c82728dca39eb4b9154a1d0
geotrieu <trieugeorge@gmail.com>
2021-02-02 02:14:10 -0500

    Added Pagination

--- fsa-ui-dev/src/components/company.jsx
+++ fsa-ui-dev/src/components/company.jsx
@@ -1,14 +1,20 @@
+import _ from "lodash";
 import React, { Component } from "react";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import { fetchFakeCompanies } from "../services/fakeCompanyService";
 
 import CompanyTable from "./common/companyTable";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Company extends Component {
     state = {
         currentCompany: {},
         similarCompanies: [],
+        itemsCount: 0,
+        currentPage: 1,
+        pageSize: 5,
     };
 
     componentDidUpdate() {
@@ -34,16 +40,37 @@ class Company extends Component {
         companies = companies.filter(
             (c) => c.cluster === this.state.currentCompany.cluster
         );
-        this.setState({ similarCompanies: companies });
+        this.setState({
+            itemsCount: companies.length,
+            similarCompanies: companies,
+        });
     }
+
+    getSortedData = () => {
+        const sorted = _.orderBy(
+            this.state.similarCompanies,
+            ["ticker"],
+            "asc"
+        );
+        const paginated = paginate(
+            sorted,
+            this.state.currentPage,
+            this.state.pageSize
+        );
+        return paginated;
+    };
 
     handleCompanyClick = (ticker) => {
         this.props.history.push("/company/" + ticker);
     };
 
+    handlePageChange = (page) => {
+        this.setState({ currentPage: page });
+    };
+
     render() {
         let ticker = this.props.match.params.ticker;
-        let { currentCompany, similarCompanies } = this.state;
+        let { currentCompany, itemsCount, currentPage, pageSize } = this.state;
         return (
             <React.Fragment>
                 <h1>
@@ -62,9 +89,15 @@ class Company extends Component {
                         <Col>
                             <h4>Similar Companies</h4>
                             <CompanyTable
-                                companies={similarCompanies}
+                                companies={this.getSortedData()}
                                 onClick={this.handleCompanyClick}
                                 exclude={{ cluster: true }}
+                            />
+                            <Pagination
+                                onPageChange={this.handlePageChange}
+                                itemsCount={itemsCount}
+                                currentPage={currentPage}
+                                pageSize={pageSize}
                             />
                         </Col>
                     </Row>
