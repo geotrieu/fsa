ca600e7867ad3c717b71f7bd42eb2dd1df3fa345
geotrieu <trieugeorge@gmail.com>
2021-02-02 01:58:25 -0500

    adding pagination:

--- fsa-ui-dev/src/components/clusters.jsx
+++ fsa-ui-dev/src/components/clusters.jsx
@@ -1,11 +1,17 @@
+import _ from "lodash";
 import React, { Component } from "react";
 
 import { fetchFakeCompanies } from "../services/fakeCompanyService";
 import CompanyTable from "./common/companyTable";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Clusters extends Component {
     state = {
         companies: [],
+        itemsCount: 0,
+        currentPage: 1,
+        pageSize: 12,
     };
 
     componentDidMount() {
@@ -13,21 +19,44 @@ class Clusters extends Component {
     }
 
     async getFakeCompanies() {
-        let companies = await fetchFakeCompanies();
-        this.setState({ companies: companies });
+        const companies = await fetchFakeCompanies();
+        this.setState({ itemsCount: companies.length, companies: companies });
+        this.getSortedData();
     }
+
+    getSortedData = () => {
+        const sorted = _.orderBy(this.state.companies, ["ticker"], "asc");
+        const paginated = paginate(
+            sorted,
+            this.state.currentPage,
+            this.state.pageSize
+        );
+        return paginated;
+    };
 
     handleCompanyClick = (ticker) => {
         this.props.history.push("/company/" + ticker);
     };
 
+    handlePageChange = (page) => {
+        this.setState({ currentPage: page });
+    };
+
     render() {
-        let { companies } = this.state;
+        let { itemsCount, currentPage, pageSize } = this.state;
         return (
-            <CompanyTable
-                companies={companies}
-                onClick={this.handleCompanyClick}
-            />
+            <React.Fragment>
+                <CompanyTable
+                    companies={this.getSortedData()}
+                    onClick={this.handleCompanyClick}
+                />
+                <Pagination
+                    onPageChange={this.handlePageChange}
+                    itemsCount={itemsCount}
+                    currentPage={currentPage}
+                    pageSize={pageSize}
+                />
+            </React.Fragment>
         );
     }
 }
