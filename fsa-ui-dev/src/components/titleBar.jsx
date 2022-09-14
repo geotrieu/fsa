d3cf8b13a1774cae2037f7fca952e86096a1e619
geotrieu <trieugeorge@gmail.com>
2022-09-13 21:58:19 -0400

    Fixed file name caps case

---
+++ fsa-ui-dev/src/components/titleBar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+
+const TitleBar = ({ searchTerm, onChange }) => {
+    const history = useHistory();
+
+    function handleSearchClick(e) {
+        e.preventDefault();
+        history.push("/clusters");
+    }
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>QMIND FSA</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/clusters">
+                        Company Clusters
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/aboutus">
+                        About Us
+                    </Nav.Link>
+                </Nav>
+                <Form onSubmit={handleSearchClick} inline>
+                    <FormControl
+                        type="text"
+                        placeholder="Search Companies"
+                        className="mr-2 w-auto"
+                        value={searchTerm}
+                        onChange={(e) => onChange(e.currentTarget.value)}
+                    />
+                    <Button
+                        className="w-25"
+                        onClick={handleSearchClick}
+                        variant="outline-success"
+                    >
+                        Search
+                    </Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default TitleBar;
